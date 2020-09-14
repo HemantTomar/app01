f05cf23d28cab86c5746792c17138121abd7a640
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2020-09-14 22:26:32 +0530

    Update instances.tf

--- modules/ec2/instances.tf
+++ modules/ec2/instances.tf
@@ -1,14 +1,14 @@
 
 
 resource "aws_instance" "webservers" {
-	count = "${length(var.subnets_cidr)}" 
-	ami = "${var.webservers_ami}"
-	instance_type = "${var.instance_type}"
+	#count = "${length(var.subnets_cidr)}" 
+	ami = var.webservers_ami
+	instance_type = var.instance_type
 	security_groups = ["${aws_security_group.webservers.id}"]
-	subnet_id = "${element(aws_subnet.public.*.id,count.index)}"
+	subnet_id = "${aws_subnet.public.id}"
 	#user_data = "${file("install_httpd.sh")}"
 
 	tags = {
-	  Name = "Server-${count.index}"
+	  Name = "Server1"
 	}
 }
