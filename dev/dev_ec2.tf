799d4f26645b68f2f6c810aa696213592b40ba3c
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2020-09-14 22:31:29 +0530

    Update dev_ec2.tf

--- dev/dev_ec2.tf
+++ dev/dev_ec2.tf
@@ -1,7 +1,7 @@
 module "my_ec2" {
   source        = "../modules/ec2"
  # ec2_count     = 1
-  ami_id        = "ami-0c94855ba95c71c99"
+  webservers_ami        = "ami-0c94855ba95c71c99"
   instance_type = "t2.micro"
   subnet_id     = "${module.my_vpc.subnet_id}"
 }
