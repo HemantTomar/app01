9ba6cec761121f861d8f83f2be50cdbc37cb0bc6
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2020-09-14 22:09:34 +0530

    Update route_table.tf

--- modules/vpc/route_table.tf
+++ modules/vpc/route_table.tf
@@ -12,7 +12,8 @@ resource "aws_route_table" "public_rt" {
 
 # Route table association with public subnets
 resource "aws_route_table_association" "a" {
-  count = "${length(var.subnets_cidr)}"
-  subnet_id      = "${element(aws_subnet.public.*.id,count.index)}"
+  #count = "${length(var.subnets_cidr)}"
+  #subnet_id      = "${element(aws_subnet.public.*.id,count.index)}"
+  subnet_id      = "${aws_subnet.public.id}"
   route_table_id = "${aws_route_table.public_rt.id}"
 }
