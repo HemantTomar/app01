56edb4e17ac49a5d38dd23b61005d5ebd737cbe1
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2020-09-14 18:04:57 +0530

    Update internet_gatway.tf

--- modules/vpc/internet_gatway.tf
+++ modules/vpc/internet_gatway.tf
@@ -1,6 +1,6 @@
 # Internet Gateway
-resource "aws_internet_gateway" "main_igw" {
-  vpc_id = "${aws_vpc.main.id}"
+resource "aws_internet_gateway" "terra_igw" {
+  vpc_id = "${aws_vpc.terra_vpc.id}"
   tags {
     Name = "main"
   }
