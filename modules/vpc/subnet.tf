8d60c9430b7547a7338a6d6ce9b6147c894d6854
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2020-09-14 22:03:34 +0530

    Update subnet.tf

--- modules/vpc/subnet.tf
+++ modules/vpc/subnet.tf
@@ -1,11 +1,12 @@
 # Subnets : public
 resource "aws_subnet" "public" {
-  count = "${length(var.subnets_cidr)}"
+  #count = "${length(var.subnets_cidr)}"
   vpc_id = "${aws_vpc.terra_vpc.id}"
-  cidr_block = "${element(var.subnets_cidr,count.index)}"
-  availability_zone = "${element(var.azs,count.index)}"
+  #cidr_block = "${element(var.subnets_cidr,count.index)}"
+  cidr_block = var.subnets_cidr
+  availability_zone = var.azs
   map_public_ip_on_launch = true
   tags= {
-    Name = "Subnet-${count.index+1}"
+    Name = "Subnet"
   }
 }
